3ac619db4bb5b9a6b1efb3746b253c52a147130b
Daniel Sarmiento <josedanielsarmiento219@gmail.com>
2024-11-18 08:59:39 -0500

    Configure basic pdfViewerActivty

--- app/src/main/java/com/example/augmented_reality/viewmodel/PdfViewerActivity.kt
+++ app/src/main/java/com/example/augmented_reality/viewmodel/PdfViewerActivity.kt
@@ -1,2 +1,24 @@
 package com.example.augmented_reality.viewmodel
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.github.barteksc.pdfviewer.PDFView
+
+class PdfViewerActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val machineId = intent.getStringExtra("MACHINE_ID")
+        val pdfPath = "assets/$machineId/$machineId.pdf"
+
+        // Set up PDFView
+        val pdfView = PDFView(this, null)
+        setContentView(pdfView)
+
+        // Load the PDF
+        pdfView.fromAsset(pdfPath)
+            .enableSwipe(true)
+            .swipeHorizontal(false)
+            .enableDoubletap(true)
+            .load()
+    }
+}
